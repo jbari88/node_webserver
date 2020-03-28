858491bdf7c88046b6d00bb11a7ebed36920c64c
hailey-han <hailey.han@kakaocorp.com>
2020-03-28 22:02:56 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', function(req,res){
+	res.render('home');
+});
+
+app.get('/about', function(req,res){
+	res.render('about');
+});
+
+
+app.use(function(req,res){
+	//res.type('text/plain');
+	res.status(404);
+	//res.send('404 - Not Found');
+	res.render('404');
+});
+
+app.use(function(err,req,res,next){
+	console.error(err.stack);
+	//res.type('text/plain');
+	res.status(500);
+	//res.send('500 - Server Error');
+	res.render('500'); 
+
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:'
+	+ app.get('port') +'; press Ctrl-C to terminate');
+});
